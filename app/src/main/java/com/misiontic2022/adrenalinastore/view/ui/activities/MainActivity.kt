5b2d186908336e0b37cce8e868ed4056110defec
Sneyder Gantiva <esgantivar@unal.edu.co>
2021-09-16 14:00:33 -0500

    Actividades Semana 1

---
+++ app/src/main/java/com/misiontic2022/adrenalinastore/view/ui/activities/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.misiontic2022.adrenalinastore.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.misiontic2022.adrenalinastore.R
+import com.misiontic2022.adrenalinastore.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+    }
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnvMenu).setupWithNavController(navController)
+    }
+}
